0f6ed80b749cc9dac0c5a2550c585c25e6e8e253
tejasai2007 <tejasaicharan55@gmail.com>
2021-03-30 13:51:41 +0530

    playerRank

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,9 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getCarsAtEnd();
+
+
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -55,7 +57,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
+      var x = 230 ;
       var y;
 
       for(var plr in allPlayers){
@@ -93,6 +95,8 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank+=1
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +104,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,8 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
+
   }
 
   getCount(){
@@ -32,4 +34,24 @@ class Player {
       allPlayers = data.val();
     })
   }
-}
+
+  getCarsAtEnd(){
+    database.ref('carsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    })
+
+  }
+
+    static updateCarsAtEnd(rank){
+
+      database.ref('/').update({
+        carsAtEnd : rank 
+      });
+
+    }
+
+
+  }
+
+
+
